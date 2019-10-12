c0ec78c682b1032aae5c38e65e3b17bfbd59bdef
Kalina Nikolova <kalina.nikolovah@gmail.com>
2019-10-12 14:11:35 +0200

    d

--- src/DarkTichu/DarkTichu.java
+++ src/DarkTichu/DarkTichu.java
@@ -17,7 +17,7 @@ public class DarkTichu extends Application {
 	int azure = 3;
 	int a=2;
 	int z=0;
-	//commentsfffwdwd
+	//commentsfffwdwdddcc
 	public static void main(String[] args) {
 		launch();
 	}
