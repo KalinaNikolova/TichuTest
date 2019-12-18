33ff8499a3f231e6a4c69d0beeff721747deb60c
D_Ric <D_Ric@192.168.1.153>
2019-12-19 00:54:40 +0100

    test classes of Combinations

---
+++ testsrc/test/tichu/Combinations/KareBombTest.java
@@ -0,0 +1,264 @@
+/**
+ * 
+ */
+package test.tichu.Combinations;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author D_Ric
+ *
+ */
+public class KareBombTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link tichu.Combinations.KareBomb#ispowerOfSetGreaterThan(tichu.Combinations.CardCombination)}.
+	 */
+	@Test
+	public final void testIspowerOfSetGreaterThan() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link tichu.Combinations.KareBomb#isBomb()}.
+	 */
+	@Test
+	public final void testIsBomb() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link tichu.Combinations.KareBomb#sizeOfSet()}.
+	 */
+	@Test
+	public final void testSizeOfSet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link tichu.Combinations.KareBomb#KareBomb(java.util.ArrayList)}.
+	 */
+	@Test
+	public final void testKareBomb() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link tichu.Combinations.CardCombination#CardCombination(java.util.ArrayList)}.
+	 */
+	@Test
+	public final void testCardCombinationArrayListOfCard() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link tichu.Combinations.CardCombination#CardCombination()}.
+	 */
+	@Test
+	public final void testCardCombination() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link tichu.Combinations.CardCombination#getCards()}.
+	 */
+	@Test
+	public final void testGetCards() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link tichu.Combinations.CardCombination#setCards(java.util.ArrayList)}.
+	 */
+	@Test
+	public final void testSetCards() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link tichu.Combinations.CardCombination#HasMajong()}.
+	 */
+	@Test
+	public final void testHasMajong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link tichu.Combinations.CardCombination#HasPhoenix()}.
+	 */
+	@Test
+	public final void testHasPhoenix() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link tichu.Combinations.CardCombination#hasCardWithValue(int)}.
+	 */
+	@Test
+	public final void testHasCardWithValue() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link tichu.Combinations.CardCombination#compareCards(tichu.Cards.Card, tichu.Cards.Card)}.
+	 */
+	@Test
+	public final void testCompareCards() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link tichu.Combinations.CardCombination#swapCards(java.util.ArrayList, int, int)}.
+	 */
+	@Test
+	public final void testSwapCards() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link tichu.Combinations.CardCombination#sortDeck(java.util.ArrayList)}.
+	 */
+	@Test
+	public final void testSortDeck() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link tichu.Combinations.CardCombination#indexLowestCard(java.util.ArrayList, int, int)}.
+	 */
+	@Test
+	public final void testIndexLowestCard() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#Object()}.
+	 */
+	@Test
+	public final void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#getClass()}.
+	 */
+	@Test
+	public final void testGetClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#hashCode()}.
+	 */
+	@Test
+	public final void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+	 */
+	@Test
+	public final void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#clone()}.
+	 */
+	@Test
+	public final void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#toString()}.
+	 */
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notify()}.
+	 */
+	@Test
+	public final void testNotify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notifyAll()}.
+	 */
+	@Test
+	public final void testNotifyAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long)}.
+	 */
+	@Test
+	public final void testWaitLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long, int)}.
+	 */
+	@Test
+	public final void testWaitLongInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait()}.
+	 */
+	@Test
+	public final void testWait() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#finalize()}.
+	 */
+	@Test
+	public final void testFinalize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
