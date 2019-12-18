7412e230e51239f64ceee9e8d828fe1676bfc0ae
D_Ric <D_Ric@192.168.1.153>
2019-12-19 00:20:57 +0100

    test classes of client

---
+++ testsrc/test/com/dark/client/HandTypeTest.java
@@ -0,0 +1,144 @@
+/**
+ * 
+ */
+package test.com.dark.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author D_Ric
+ *
+ */
+public class HandTypeTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.dark.client.HandType#evaluateHand(java.util.ArrayList)}.
+	 */
+	@Test
+	public final void testEvaluateHand() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.dark.client.HandType#isOnePair(java.util.ArrayList)}.
+	 */
+	@Test
+	public final void testIsOnePair() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.dark.client.HandType#isTwoPair(java.util.ArrayList)}.
+	 */
+	@Test
+	public final void testIsTwoPair() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.dark.client.HandType#isThreeOfAKind(java.util.ArrayList)}.
+	 */
+	@Test
+	public final void testIsThreeOfAKind() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.dark.client.HandType#isStraight(java.util.ArrayList)}.
+	 */
+	@Test
+	public final void testIsStraight() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.dark.client.HandType#isFlush(java.util.ArrayList)}.
+	 */
+	@Test
+	public final void testIsFlush() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.dark.client.HandType#isFullHouse(java.util.ArrayList)}.
+	 */
+	@Test
+	public final void testIsFullHouse() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.dark.client.HandType#isFourOfAKind(java.util.ArrayList)}.
+	 */
+	@Test
+	public final void testIsFourOfAKind() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.dark.client.HandType#isStraightFlush(java.util.ArrayList)}.
+	 */
+	@Test
+	public final void testIsStraightFlush() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.dark.client.HandType#isRoyalFlush(java.util.ArrayList)}.
+	 */
+	@Test
+	public final void testIsRoyalFlush() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.dark.client.HandType#getValueList(java.util.ArrayList)}.
+	 */
+	@Test
+	public final void testGetValueList() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.dark.client.HandType#findSameOfAKind(java.util.ArrayList, int)}.
+	 */
+	@Test
+	public final void testFindSameOfAKind() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ testsrc/test/com/dark/client/WinnerTest.java
@@ -0,0 +1,256 @@
+/**
+ * 
+ */
+package test.com.dark.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author D_Ric
+ *
+ */
+public class WinnerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.dark.client.Winner#getIndex()}.
+	 */
+	@Test
+	public final void testGetIndex() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.dark.client.Winner#setIndex(int)}.
+	 */
+	@Test
+	public final void testSetIndex() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.dark.client.Winner#getCards()}.
+	 */
+	@Test
+	public final void testGetCards() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.dark.client.Winner#setCards(java.util.ArrayList)}.
+	 */
+	@Test
+	public final void testSetCards() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.util.Observable#Observable()}.
+	 */
+	@Test
+	public final void testObservable() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.util.Observable#addObserver(java.util.Observer)}.
+	 */
+	@Test
+	public final void testAddObserver() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.util.Observable#deleteObserver(java.util.Observer)}.
+	 */
+	@Test
+	public final void testDeleteObserver() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.util.Observable#notifyObservers()}.
+	 */
+	@Test
+	public final void testNotifyObservers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.util.Observable#notifyObservers(java.lang.Object)}.
+	 */
+	@Test
+	public final void testNotifyObserversObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.util.Observable#deleteObservers()}.
+	 */
+	@Test
+	public final void testDeleteObservers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.util.Observable#setChanged()}.
+	 */
+	@Test
+	public final void testSetChanged() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.util.Observable#clearChanged()}.
+	 */
+	@Test
+	public final void testClearChanged() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.util.Observable#hasChanged()}.
+	 */
+	@Test
+	public final void testHasChanged() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.util.Observable#countObservers()}.
+	 */
+	@Test
+	public final void testCountObservers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#Object()}.
+	 */
+	@Test
+	public final void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#getClass()}.
+	 */
+	@Test
+	public final void testGetClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#hashCode()}.
+	 */
+	@Test
+	public final void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+	 */
+	@Test
+	public final void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#clone()}.
+	 */
+	@Test
+	public final void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#toString()}.
+	 */
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notify()}.
+	 */
+	@Test
+	public final void testNotify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notifyAll()}.
+	 */
+	@Test
+	public final void testNotifyAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long)}.
+	 */
+	@Test
+	public final void testWaitLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long, int)}.
+	 */
+	@Test
+	public final void testWaitLongInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait()}.
+	 */
+	@Test
+	public final void testWait() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#finalize()}.
+	 */
+	@Test
+	public final void testFinalize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
